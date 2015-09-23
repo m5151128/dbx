46e06c30ba0bde282876b73f1a7762c207f03c6c
m5151128 <m5151128@gmail.com>
2015-09-23 13:45:55 +0900

    create get accunt info class

---
+++ getAccountInfo.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'dbxClient.php';
+
+class getAccountInfo extends dbxClient {
+
+    function getData() {
+        return $this->dbxClient->getAccountInfo();
+    }
+}
+
+print_r((new getAccountInfo())->getData());
