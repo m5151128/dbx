e9b1a3fd0d0dc8901b13123fb735314c072246db
m5151128 <m5151128@gmail.com>
2015-09-23 14:29:46 +0900

    create donload file class

---
+++ downloadFile.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'dbxClient.php';
+use \Dropbox as dbx;
+
+class downloadFile extends dbxClient {
+
+    function download($dropboxPath, $downloadPath = null, $fileName) {
+        $f = fopen($downloadPath . $fileName, "w+b");
+        $metadata = $this->dbxClient->getFile($dropboxPath . $fileName, $f);
+        fclose($f);
+
+        return $metaData;
+    }
+}
+
+$fileName = 'upload.jpg';
+$downloadPath = 'files/';
+$dropboxPath = '/';
+
+print_r((new downloadFile())->download($dropboxPath, $downloadPath, $fileName));
